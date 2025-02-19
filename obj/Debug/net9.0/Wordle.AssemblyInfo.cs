7540831b330305fbdfb0c9138684ac180d65b084
Reeves Farrell <Reeves.Farrell@moovila.com>
2025-02-19 14:46:41 -0500

    Add gitignore

--- obj/Debug/net9.0/Wordle.AssemblyInfo.cs
+++ obj/Debug/net9.0/Wordle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wordle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95b38a1d9fe3825a05d7f6e3aac1adbbc8218e84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wordle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wordle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
